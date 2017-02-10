22dd755a1859e30dff16bb2641187255c1af95ba
liuranchao <lrc19860926@gmail.com>
2017-02-10 16:44:02 +0800

    增加ImageView测试

--- app/src/main/java/com/lrchao/lrchaoviewssample/MainActivity.java
+++ app/src/main/java/com/lrchao/lrchaoviewssample/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.lrchao.lrchaoviewssample.imageview.ImageViewActivity;
 import com.lrchao.lrchaoviewssample.tab.MainTabActivity;
 import com.lrchao.views.LrchaoViews;
 
@@ -15,6 +16,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         LrchaoViews.getInstance().init(this);
         findViewById(R.id.btn_tab).setOnClickListener(this);
+        findViewById(R.id.btn_image_view).setOnClickListener(this);
+
     }
 
     @Override
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch (v.getId()) {
             case R.id.btn_tab:
                 startActivity(new Intent(this, MainTabActivity.class));
+                break;
+            case R.id.btn_image_view:
+                startActivity(new Intent(this, ImageViewActivity.class));
                 break;
             default:
                 break;
---
+++ app/src/main/java/com/lrchao/lrchaoviewssample/imageview/ImageViewActivity.java
@@ -0,0 +1,15 @@
+package com.lrchao.lrchaoviewssample.imageview;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.lrchao.lrchaoviewssample.R;
+
+public class ImageViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_view);
+    }
+}
