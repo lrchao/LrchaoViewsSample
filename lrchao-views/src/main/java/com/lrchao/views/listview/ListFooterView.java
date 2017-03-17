3fc018d52c6987d608cb06e55f624c8c04246abe
liuranchao <lrc19860926@gmail.com>
2017-03-17 14:03:02 +0800

    footer增加点击，可失败的文本

--- lrchao-views/src/main/java/com/lrchao/views/listview/ListFooterView.java
+++ lrchao-views/src/main/java/com/lrchao/views/listview/ListFooterView.java
@@ -19,7 +19,7 @@ import static com.lrchao.views.R.id.tv_footer;
  * @author liuranchao
  * @date 16/2/29 下午1:25
  */
-public class ListFooterView extends RelativeLayout {
+public class ListFooterView extends RelativeLayout implements View.OnClickListener {
 
     /**
      * Loading
@@ -30,6 +30,14 @@ public class ListFooterView extends RelativeLayout {
      * 没有更多数据
      */
     private TextView mTvContent;
+
+    private boolean mIsClickable;
+
+    private OnFooterClickListener mOnFooterClickListener;
+
+    public void setOnFooterClickListener(OnFooterClickListener onFooterClickListener) {
+        mOnFooterClickListener = onFooterClickListener;
+    }
 
     public ListFooterView(Context context) {
         super(context);
@@ -43,27 +51,62 @@ public class ListFooterView extends RelativeLayout {
 
     private void init(Context context) {
         LayoutInflater.from(context).inflate(R.layout.list_footer, this);
+        setOnClickListener(this);
         mProgressBar = (ProgressBar) findViewById(R.id.pb_footer);
         mTvContent = (TextView) findViewById(tv_footer);
     }
 
+    private String mNoMoreDataText;
+    private String mErrorText;
+
     public void setNoMoreDataText(String text) {
-        mTvContent.setText(text);
+        mNoMoreDataText = text;
+
+    }
+
+    public void setErrorText(String text) {
+        mErrorText = text;
     }
 
     /**
      * 显示没有更多数据
      */
     public void showNoMoreData() {
+        mIsClickable = true;
         mProgressBar.setVisibility(View.GONE);
         mTvContent.setVisibility(View.VISIBLE);
+        mTvContent.setText(mNoMoreDataText);
+    }
+
+    /**
+     * 设置失败的显示
+     */
+    public void showError() {
+        mIsClickable = true;
+        mProgressBar.setVisibility(View.GONE);
+        mTvContent.setVisibility(View.VISIBLE);
+        mTvContent.setText(mErrorText);
     }
 
     /**
      * 显示正在加载中
      */
     public void showLoading() {
+        mIsClickable = false;
         mProgressBar.setVisibility(View.VISIBLE);
         mTvContent.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (mOnFooterClickListener != null && mIsClickable) {
+            mOnFooterClickListener.onFooterClick();
+        }
+
+    }
+
+    public interface OnFooterClickListener {
+        void onFooterClick();
     }
 }
